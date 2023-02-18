d7321ffef0ce65539377bd5808317088cfd3e5f5
Lalit Dumka <lalitdumka128@gmail.com>
2023-02-18 19:23:52 +0530

    Added login/ logout feature and new user registration. Also added PostgreSQL in place in SQlite

--- home/views.py
+++ home/views.py
@@ -1,8 +1,6 @@
 from django.contrib import messages
-from django.contrib.auth import authenticate,logout, login
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
-from django.shortcuts import render, HttpResponse,redirect
+# from django.contrib.auth.models import User
+from django.shortcuts import render, HttpResponse
 	
 def index(request):
 	# context is dictionary using which we can send variables to an html file
@@ -14,32 +12,6 @@ def index(request):
     return render(request,'index.html')
     # return HttpResponse("this is homepage")
 
-def loginUser(request):
-    if request.user.is_authenticated:
-        return redirect('/')
-
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(username=username, password = password)
-        if user is not None:
-            login(request,user)
-            messages.success(request, 'Logged in !')
-            return redirect("/")
-        else:
-            messages.warning(request, 'Invalid Credentials !')
-            return render(request, 'login.html')
-    return render(request, 'login.html')
-
-
-@login_required
-def logoutUser(request):
-    logout(request)
-    messages.warning(request, "Logged out successfully!")
-    return redirect("homepage")
-
-def signupUser(request):
-    return render(request,'signup.html')
 def listProperty(request):
     # return render(request,'listProperty.html')
     return HttpResponse("this is list  property page")
